51512f86467aafe6c1c58aa17f4b1c7f280e4f88
Tkwes <wxnaider@gmail.com>
2021-04-18 18:41:07 -0300

    conjunto
    finalizado e comentado

---
+++ Assets/TarefaWaypoints/Scripts/WPManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct Link
+{
+    public enum direction { UNI, BI}
+    public GameObject node1;
+    public GameObject node2;
+    public direction dir;
+}
+public class WPManager : MonoBehaviour
+{
+    // variaveis
+    public GameObject[] waypoints; // list de pontos 
+    public Link[] links; //links de direcionamento 
+    public Graph graph = new Graph(); //pega o componente
+
+    void Start()
+    {
+        //troca de pontos apos chegadas 
+        if(waypoints.Length > 0)
+        {
+            foreach(GameObject wp in waypoints)
+            {
+                graph.AddNode(wp);
+            }
+            foreach(Link l in links)
+            {
+                graph.AddEdge(l.node1, l.node2);
+                if(l.dir == Link.direction.BI)
+                {
+                    graph.AddEdge(l.node2, l.node1);
+                }
+            }
+        }
+    }
+
+    void Update()
+    {
+        //Cria a linha 
+        graph.debugDraw();
+    }
+}
